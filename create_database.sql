5d603ffa9f489cab87de781ad4979c22f7eff611
Diofik <128151621+DionFikins@users.noreply.github.com>
2025-02-21 19:04:57 +0300

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,26 +1,12 @@
 BEGIN;
 
-
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id_user serial NOT NULL,
-    login_user text NOT NULL,
-    pass_user text NOT NULL,
-    name_user text NOT NULL,
-    enable_user boolean NOT NULL DEFAULT true,
-    PRIMARY KEY (id_user)
-);
-
-COMMENT ON TABLE public.users
-    IS 'Users data';
-
 CREATE TABLE IF NOT EXISTS public.all_chat
 (
     id_message serial NOT NULL,
     id_user integer NOT NULL,
-    message_chatall text NOT NULL,
-    deleted_message_chatall boolean NOT NULL,
-    PRIMARY KEY (id_message)
+    message_chatall text COLLATE pg_catalog."default" NOT NULL,
+    deleted_message_chatall boolean NOT NULL DEFAULT false,
+    CONSTRAINT all_chat_pkey PRIMARY KEY (id_message)
 );
 
 COMMENT ON TABLE public.all_chat
@@ -32,8 +18,7 @@ CREATE TABLE IF NOT EXISTS public.chat_users
     id_useroutput integer NOT NULL,
     id_userinput integer NOT NULL,
     deleted_chat boolean NOT NULL DEFAULT false,
-    PRIMARY KEY (id_chat),
-    UNIQUE (id_chat)
+    CONSTRAINT chat_users_pkey PRIMARY KEY (id_chat)
 );
 
 COMMENT ON TABLE public.chat_users
@@ -44,15 +29,26 @@ CREATE TABLE IF NOT EXISTS public.msg_usr_chat
     id_message_chat serial NOT NULL,
     id_chat integer NOT NULL,
     id_user_upload integer NOT NULL,
-    message_chat text NOT NULL,
+    message_chat text COLLATE pg_catalog."default" NOT NULL,
     deleted_message boolean NOT NULL DEFAULT false,
-    PRIMARY KEY (id_message_chat),
-    UNIQUE (id_message_chat),
-    UNIQUE (id_chat)
+    CONSTRAINT msg_usr_chat_pkey PRIMARY KEY (id_message_chat)
 );
 
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id_user serial NOT NULL,
+    login_user text COLLATE pg_catalog."default" NOT NULL,
+    pass_user text COLLATE pg_catalog."default" NOT NULL,
+    name_user text COLLATE pg_catalog."default" NOT NULL,
+    enable_user boolean NOT NULL DEFAULT true,
+    CONSTRAINT users_pkey PRIMARY KEY (id_user)
+);
+
+COMMENT ON TABLE public.users
+    IS 'Users data';
+
 ALTER TABLE IF EXISTS public.all_chat
-    ADD FOREIGN KEY (id_user)
+    ADD CONSTRAINT all_chat_id_user_fkey FOREIGN KEY (id_user)
     REFERENCES public.users (id_user) MATCH SIMPLE
     ON UPDATE CASCADE
     ON DELETE CASCADE
@@ -60,7 +56,7 @@ ALTER TABLE IF EXISTS public.all_chat
 
 
 ALTER TABLE IF EXISTS public.chat_users
-    ADD FOREIGN KEY (id_useroutput)
+    ADD CONSTRAINT chat_users_id_userinput_fkey FOREIGN KEY (id_userinput)
     REFERENCES public.users (id_user) MATCH SIMPLE
     ON UPDATE CASCADE
     ON DELETE CASCADE
@@ -68,7 +64,7 @@ ALTER TABLE IF EXISTS public.chat_users
 
 
 ALTER TABLE IF EXISTS public.chat_users
-    ADD FOREIGN KEY (id_userinput)
+    ADD CONSTRAINT chat_users_id_useroutput_fkey FOREIGN KEY (id_useroutput)
     REFERENCES public.users (id_user) MATCH SIMPLE
     ON UPDATE CASCADE
     ON DELETE CASCADE
@@ -76,7 +72,7 @@ ALTER TABLE IF EXISTS public.chat_users
 
 
 ALTER TABLE IF EXISTS public.msg_usr_chat
-    ADD FOREIGN KEY (id_chat)
+    ADD CONSTRAINT msg_usr_chat_id_chat_fkey FOREIGN KEY (id_chat)
     REFERENCES public.chat_users (id_chat) MATCH SIMPLE
     ON UPDATE CASCADE
     ON DELETE CASCADE
